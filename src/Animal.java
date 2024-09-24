7e7017ab278e5acdcf7d2d21f74c5400f60d2d06
souha-mejri <139564616+souha-mejri@users.noreply.github.com>
2024-09-24 10:31:01 +0100

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,10 +1,10 @@
 public class Animal {
-    private String family;
-    private String name;
-    private int age;
-    private boolean isMammal;
+ String family;
+     String name;
+     int age;
+     boolean isMammal;
 
-    // Constructeur paramétré
+    
     public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
         this.name = name;
@@ -12,7 +12,7 @@ public class Animal {
         this.isMammal = isMammal;
     }
 
-    // Getters et Setters (facultatif)
+   
     public String getFamily() {
         return family;
     }
